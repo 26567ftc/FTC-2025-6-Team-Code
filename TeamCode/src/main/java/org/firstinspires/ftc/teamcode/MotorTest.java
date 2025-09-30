0d2d4f9b6aa6a128e1ce10887101ab32e8327404
ProgramingSquid <141872768+ProgramingSquid@users.noreply.github.com>
2025-09-29 19:52:51 -0700

    Updated MotorTest OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -3,14 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="MotorTest_LinearOpMode", group="Robot")
 public class MotorTest extends LinearOpMode {
 
     private DcMotor leftFrontMotor = null;
-    private DcMotor leftBackMotor = null;
     private DcMotor rightFrontMotor = null;
-    private DcMotor rightBackMotor = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -26,32 +25,20 @@ public class MotorTest extends LinearOpMode {
 
         /* Run until the driver presses stop */
         while (opModeIsActive()) {
-            double power = gamepad1.left_stick_y;
-
-            if(gamepad1.left_bumper)
-                leftFrontMotor.setPower(power);
-            if(gamepad1.right_bumper)
-                rightFrontMotor.setPower(power);
-            if(gamepad1.left_trigger > 0.5)
-                leftFrontMotor.setPower(power);
-            if(gamepad1.right_trigger > 0.5)
-                leftFrontMotor.setPower(power);
+                leftFrontMotor.setPower(gamepad1.right_trigger);
+                rightFrontMotor.setPower(gamepad1.right_trigger);
         }
     }
 
     private void setMotorDirections() {
         leftFrontMotor.setDirection(
                 DcMotor.Direction.REVERSE);
-        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
         rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
-        rightBackMotor.setDirection(DcMotor.Direction.FORWARD);
     }
 
     private void initMotorHardware() {
         // Initialize the hardware variables. Note that the strings used here must correspond
         leftFrontMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
-        leftBackMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
         rightFrontMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
-        rightBackMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
     }
 }
